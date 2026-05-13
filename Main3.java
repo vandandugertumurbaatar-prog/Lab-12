dcd46766b9c0a6846e59c0d026cea89083762e16
vandandugertumurbaatar-prog <vandandugertumurbaatar@gmail.com>
2026-05-13 09:06:53 +0800

    Implement cloning for UnderGraduateStudent
    Added cloning functionality for UnderGraduateStudent.

--- Main3.java
+++ Main3.java
@@ -5,13 +5,13 @@ public class Main3 {
         UnderGraduateStudent s1 = new UnderGraduateStudent("Saruul", scores);
         
         UnderGraduateStudent s2 = (UnderGraduateStudent) s1.clone();
+
+        System.out.println("Original Student Name: " + s1.getName());
+        System.out.println("Cloned Student Name: " + s2.getName());
+
         s2.setName("Temuulen");
 
         System.out.println("Original Student Name: " + s1.getName());
         System.out.println("Cloned Student Name: " + s2.getName());
-        
-        if (s1 != s2) {
-            System.out.println("Result: Success! Cloned object has a different memory address.");
-        }
     }
 }
